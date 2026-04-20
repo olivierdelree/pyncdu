4ae64eb2aa87b1f67d6dd9c9d30bed2eb5f74352
Olivier Delrée <olivierdelree@protonmail.com>
2026-04-20 16:37:17 +0100

    CBOR: Add CBOR parsing logic

---
+++ src/pyncdu/errors.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: © 2026 Olivier Delrée <olivierdelree@protonmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+"""Custom errors."""
+
+import abc
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import pathlib
+
+
+class ParsingError(Exception, abc.ABC):
+    """Parsing of an NCDU file failed."""
+
+    _reason: str
+    _path: str | None
+
+    def __init__(self, reason: str, path: str | pathlib.Path | None = None) -> None:
+        path_string = "" if path is None else f" ({path})"
+        message = f"Parsing failed: {reason}{path_string}"
+
+        super().__init__(message)
+
+        self._reason = reason
+        self._path = path if path is None else str(path)
+
+    @property
+    def reason(self) -> str:
+        """Reason for the exception."""
+        return self._reason
+
+    @property
+    def path(self) -> str | None:
+        """Path to the file whose content caused the exception."""
+        return self._path
+
+
+class CBORError(ParsingError):
+    """CBOR failed to parse data."""
